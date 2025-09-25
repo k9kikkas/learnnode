7726359cde7c98827350f66b291a2b8021b6b92c
Marta Kikkas <marta.kikkas@tptlive.ee>
2025-09-25 13:36:20 +0300

    add string (meth)ods examples

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-let answer = 1 + 1;
-answer = 1 - 1;
-answer = 2 * 3;
-answer = 2 / 3;
-answer = 7 % 2;
-answer = 2 ** 3;
-console.log(answer);
-let num = 1;
-num = num + 1;
-num += 1;
-num -= 1;
-num *= 2;
-num /= 3;
-num %= 2;
-num **= 2;
-num++;
-num--;
-num = 1;
-num = Math.pow(3, 4);
-console.log(num);
+let text = 'Hello ';
+text = text + 'Joosep!';
+console.log(text);
+text = "Don't use double quotes unless needed";
+text = 'Don\'t use double\t quotes\n unless needed';
+console.log(text);
+
+let name = 'Tide';
+let age = 27;
+let sexuality = 'gay';
+text = 'Your name is ' + name + ' and you are ' + age + ' years old and your sexuality is ' + sexuality + '!';
+console.log(text);
+
+text = `Your name is ${name}
+ and you are ${age} years old
+  and your sexuality is ${sexuality}!`;
+console.log(text);
+
+console.log(text.toUpperCase());
+text = text.toUpperCase();
+console.log(text);
