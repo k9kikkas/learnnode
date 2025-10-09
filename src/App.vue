8d3b4b074665646ed335758750d60a05afb894ab
Marta Kikkas <marta.kikkas@tptlive.ee>
2025-10-09 16:53:23 +0300

    add simple item list example with vue

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref } from "vue";
+
+let newItem = ref("");
+let items = ref(["Leib", "Vorst", "Krõps"]);
+let code = ref(404);
+
+function addItem() {
+  items.value.push(newItem.value);
+  newItem.value = "";
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="content">
+      <div class="field has-addons">
+        <div class="control is-expanded">
+          <input
+            v-model="newItem"
+            class="input"
+            type="text"
+            placeholder="Add item"
+          />
+        </div>
+        <div class="control">
+          <button class="button is-info" @click="addItem">Add item</button>
+        </div>
+      </div>
+      <h1>{{ newItem }}</h1>
+      <ul>
+        <li v-for="item in items">{{ item }}</li>
+      </ul>
+      <input
+        v-model="code"
+        class="input"
+        type="number"
+        placeholder="Enter error code"
+      />
+      <img v-bind:src="'https://http.cat/' + code" />
+    </div>
+  </div>
+</template>
