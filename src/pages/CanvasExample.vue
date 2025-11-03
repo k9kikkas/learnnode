db41835fb72f4b337e45d284f0fe17944a3819d5
Marta Kikkas <marta.kikkas@tptlive.ee>
2025-11-03 15:56:00 +0200

    canvas mouse follow ball color

--- src/pages/CanvasExample.vue
+++ src/pages/CanvasExample.vue
@@ -2,41 +2,53 @@
 import { onMounted } from 'vue';
 let x = 95;
 let y = 50;
-
+let balls = [];
 
 onMounted(() => {
     let mycanvas = document.querySelector('#mycanvas');
-    console.log(mycanvas);
     let ctx = mycanvas.getContext('2d');
 
     setInterval(() => {
+        balls.map(ball => {
+            return {
+                x: ball.x, 
+                y: ball.y,
+                radius: ball.radius-5,
+                red: ball.red-5,
+                green: ball.green+5,
+                blue: 0,
+            }
+        }).filter(ball => ball.radius > 0);
+
+        balls.push({
+            x: x, 
+            y: y,
+            size: 40,
+            red: 255,
+            green: 0,
+            blue: 0,
+        });
+
         ctx.clearRect(0, 0, 1600, 800);
-        ctx.beginPath();
-        ctx.arc(x, y, 40, 0, 2 * Math.PI);
-        ctx.fillStyle = '#112A5D';
-        ctx.fill();
+
+        balls.forEach(ball => {
+            
+            ctx.beginPath();
+            ctx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
+            ctx.fillStyle = `rgb(${ball.red}, ${ball.green}, ${ball.blue})`;
+            ctx.fill();
+        });
     }, 16);
-    
 });
 
-function keydown(event){
+function mousemove(event){
     console.log(event);
-    if(event.code === 'KeyD') {
-        x+=5;
-    };
-    if(event.code === 'KeyA') {
-        x-=5;
-    };
-    if(event.code === 'KeyS') {
-        y+=5;
-    };
-    if(event.code === 'KeyW') {
-        y-=5;
-    };
-};
+    x = event.offsetX;
+    y = event.offsetY;
+}
 
 </script>
 
 <template>
-    <canvas id="mycanvas" tabindex="1" width="1600" height="800" @keydown="keydown"></canvas>
+    <canvas id="mycanvas" tabindex="1" width="1600" height="800" @mousemove="mousemove"></canvas>
 </template>
