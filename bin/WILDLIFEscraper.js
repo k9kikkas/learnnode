0362a1c96e953af1a82ecf129221ef34b25c7865
Marta Kikkas <marta.kikkas@tptlive.ee>
2025-11-27 16:55:59 +0200

    wildelife comic scraper

---
+++ bin/WILDLIFEscraper.js
@@ -0,0 +1,29 @@
+import * as cheerio from 'cheerio';
+import fs from 'fs';
+import md5 from 'md5';
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+let cacheDir = import.meta.dirname + '/../cache';
+
+if(!fs.existsSync(cacheDir)){
+    fs.mkdirSync(cacheDir);
+}
+
+const BASE_URL = 'https://www.wildelifecomic.com';
+let url = BASE_URL + '/comic';
+for(let i = 0; i<10; i++){
+    let body;
+    if(fs.existsSync(cacheDir + `/${i}.html`)){
+        body = fs.readFileSync(cacheDir + `/${i}.html`);
+    } else {
+        await sleep(1000);    
+        let res = await fetch(url);    
+        body = await res.text();
+        fs.writeFileSync(cacheDir + `/${i}.html`, body);
+    }
+
+    const $ = cheerio.load(body);
+    let img = $('cc-comic>img');
+    console.log(img.attr('src'));
+    console.log(img.attr('alt'));
+    console.log(img.attr('title'));
+}
